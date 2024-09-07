9449539e226f7f4e28275a5462e32b3560a07040
Celia <elioandre692@gmail.com>
2024-09-07 23:02:05 +0200

    feat: ex02 finished and ex03 in progress

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 22:27:57 by eandre            #+#    #+#             */
+/*   Updated: 2024/09/07 22:50:58 by eandre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iomanip>
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "\033[0;34mAddresses:\033[0m" << std::endl;
+	std::cout << "str address value:" << std::setw(21) << &str << std::endl;
+	std::cout << "stringPTR address value: " << stringPTR << std::endl;
+	std::cout << "stringREF address value: " << &stringREF << std::endl;
+
+	std::cout << "\033[0;34mValues:\033[0m" << std::endl;
+	std::cout << "str value:" << std::setw(23) << str << std::endl;
+	std::cout << "stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "stringREF value: " << stringREF << std::endl;
+}
