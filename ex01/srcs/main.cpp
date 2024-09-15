d9dfe42561477d6d3f8d29789091afce77d2ce3b
Celia <elioandre692@gmail.com>
2024-09-15 16:59:40 +0200

    update: added tests on main

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 14:27:35 by eandre            #+#    #+#             */
-/*   Updated: 2024/09/07 14:59:06 by eandre           ###   ########.fr       */
+/*   Updated: 2024/09/15 16:58:38 by eandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 int	main(int argc, char **argv)
 {
 	Zombie	*zombie_horde;
+	int		i;
 
+	i = 0;
 	if (argc > 1)
 		zombie_horde = zombieHorde(10, argv[1]);
 	else
 		zombie_horde = zombieHorde(10, "Stephane");
+	while (i < 10)
+		zombie_horde[i++].announce();
 	delete [] zombie_horde;
 }
